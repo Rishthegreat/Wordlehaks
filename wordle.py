656d6edbdf6a3877cbffeb09e9f31c0b26241b73
Rishit Piano <76021793+Rishthegreat@users.noreply.github.com>
2022-02-24 16:07:23 +0000

    adding stuff

--- wordle.py
+++ wordle.py
@@ -1,11 +1,38 @@
+import sys
+global words
+
 with open('words.txt') as f:
+  global words
   words = f.readlines()
 
 for i in range (0, len(words)):
   words[i] = words[i][:-1]
   words[i] = words[i].lower()
 
-for i in range (0, len(words)):
-  if len(words[i]) > 5:
-    words.pop(i)
-print(words[0:20])
+wordstemp = []
+for word in words:
+  if (len(word) == 5) & word.isalpha():
+    wordstemp.append(word)
+words = wordstemp
+
+def rem(letter):
+  global words
+  wordstemp = []
+  for word in words:
+    if letter not in word:
+      wordstemp.append(word)
+  words = wordstemp
+
+if __name__ == "__main__":
+  args = sys.argv[1:]
+  if len(args) <= 0:
+    print(words[0].isalpha())
+  elif args[0] == "-w":
+    given = args[1]
+  elif args[0] == "-r":
+    lettersToRemove = []
+    i = 1
+    print(len(args))
+    while ((i < len(args)) & ('-' not in args[i])):
+      lettersToRemove.append(args[i])
+      i+=1    
